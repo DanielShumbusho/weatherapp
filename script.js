5c4adc120f39269172e0e07656d3c8de563bfadd
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-04-26 17:31:25 +0200

    final js imlementation and debuging

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function getWeatherResult(city) {
             if(!response.ok) {
                 throw new Error("City not found");
             }
-            response.json()
+            return response.json()
         })
         .then(data => {
             displayWeather(data);
@@ -32,4 +32,49 @@ function getWeatherResult(city) {
         .catch(error => {
             weatherResult.innerHTML = `<p>${error.message}</p>`;
         });
+}
+
+function displayWeather(data) {
+    const temp = data.main.temp;
+    const description = data.weather[0].description;
+    const humidity = data.main.humidity;
+    const cityName = data.name;
+    const country = data.sys.country;
+    
+
+    const flagUrl = `https://flagsapi.com/${country}/flat/64.png`;
+    weatherResult.innerHTML = `
+    <h2>
+        ${cityName}, ${country}
+        <img src="${flagUrl}" alt="Flag of ${country}" style="width: 40px; vertical-align: middle;">
+    </h2>
+
+    <p><strong>Temperature:</strong> ${temp}°C</p>
+    <p><strong>Condition:</strong> ${description}</p>
+    <p><strong>Humidity:</strong> ${humidity}</p>
+
+    <div id="PostResult" class="post-resut">
+    <h3>Sample Posts</h3>
+        <ul id="postList"></ul>
+    </div>
+    `;
+  
+}
+function fetchPost() {
+    const postUrl = "https://jsonplaceholder.typicode.com/posts?_limit=2";
+    fetch(postUrl)
+    .then (response => response.json())
+    .then(posts => {
+        const postsList = document.getElementById("postList");
+
+        posts.forEach((post) => {
+            const li = document.createElement("li");
+            li.innerHTML = `<strong>${post.title} </strong> <br> ${post.body}`;
+            postsList.appendChild(li);
+        });
+    })
+    .catch(error => {
+        const PostResult = document.getElementById("PostResult");
+        PostResult.innerHTML = `<p>Failed to load post ${error.message}</p>`
+    })
 }
